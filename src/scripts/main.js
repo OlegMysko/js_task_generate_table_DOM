7c6134123cb155aac6e168bd9807cd8a80359dba
Oleh Mysko <olegmysko1996@gmail.com>
2025-01-19 15:13:51 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -354,7 +354,43 @@ const people = [
   },
 ];
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const tablet = document.querySelector('.dashboard');
 
-// write your code here
+people.forEach((person) => {
+  const age = person.died - person.born;
+  const centry = Math.ceil(person.died / 100);
+
+  const row = document.createElement('tr');
+  const nameCell = document.createElement('th');
+
+  nameCell.textContent = person.name;
+
+  const genderCell = document.createElement('th');
+
+  genderCell.textContent = person.sex;
+
+  const bornCell = document.createElement('th');
+
+  bornCell.textContent = person.born;
+
+  const diedCell = document.createElement('th');
+
+  diedCell.textContent = person.died;
+
+  const ageCell = document.createElement('th');
+
+  ageCell.textContent = age;
+
+  const centryCell = document.createElement('th');
+
+  centryCell.textContent = centry;
+
+  row.appendChild(nameCell);
+  row.appendChild(genderCell);
+  row.appendChild(bornCell);
+  row.appendChild(diedCell);
+  row.appendChild(ageCell);
+  row.appendChild(centryCell);
+
+  tablet.appendChild(row);
+});
